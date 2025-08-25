f2fa6835c3efcdecde9a06f87623dcb38f9063ab
Malkiel Ben-Ezra <052840@gmail.com>
2025-08-25 22:28:47 +0300

    index3

--- auth/login.php
+++ auth/login.php
@@ -331,7 +331,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['register'])) {
     <?php echo getPWAScripts([
         'banner_type' => 'custom',
         'page_type' => 'login',
-        'show_after_seconds' => 1,
+        // 'show_after_seconds' => 1,
         'title' => 'הפוך אותנו לאפליקציה!',
         'subtitle' => 'התקנה מהירה, גישה נוחה'
     ]); ?>

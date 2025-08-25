bfdcbb6e769db6681d7475c58a10bbd9c04db858
Malkiel Ben-Ezra <052840@gmail.com>
2025-08-25 16:52:50 +0300

    index1

--- index.php
+++ index.php
@@ -4,18 +4,17 @@
  * index.php
  */
 
-// session_start();
+session_start();
 
 // אם המשתמש מחובר - הפנה לדשבורד
-// if (isset($_SESSION['user_id'])) {
-//     header('Location: dashboard.php');
-//     exit;
-// }
+if (isset($_SESSION['user_id'])) {
+    header('Location: dashboard.php');
+    exit;
+}
 
 // אם לא מחובר - הפנה לדף התחברות
-// header('Location: /auth/login.php');
+header('Location: auth/login.php');
 
 
-// exit;
+exit;
 ?>
-ffff

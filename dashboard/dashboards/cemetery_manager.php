1723a433f07ebcb7043495043ca825ef8ba06bf0
Malkiel Ben-Ezra <052840@gmail.com>
2025-09-03 17:21:48 +0300

    app12

--- dashboard/dashboards/cemetery_manager.php
+++ dashboard/dashboards/cemetery_manager.php
@@ -1,5 +1,5 @@
 <?php
 // פשוט כלול את הקובץ במקום להפנות
-include 'cemeteries/test-api.php';
-// include 'cemeteries/index.php';
+// include 'cemeteries/test-api.php';
+include 'cemeteries/index.php';
 ?>

ee45241d54aa6236f6f2696a12e1c00f59a294d4
Malkiel Ben-Ezra <052840@gmail.com>
2025-09-15 19:16:48 +0300

    app18 cemeteries forms v1.21.5

--- dashboard/dashboards/cemeteries/forms/purchase-form.php
+++ dashboard/dashboards/cemeteries/forms/purchase-form.php
@@ -295,6 +295,17 @@
     // הצג את הטופס
     echo $formBuilder->renderModal();
 
+
+// דיבוג - בדוק מה מוחזר
+if (strpos($modalHTML, 'purchaseFormModal') === false) {
+    // אם אין את ה-ID הנכון, עטוף ידנית
+    echo '<div id="purchaseFormModal" class="modal fade" tabindex="-1">';
+    echo $modalHTML;
+    echo '</div>';
+} else {
+    echo $modalHTML;
+}
+
 ?>
 
 <script>

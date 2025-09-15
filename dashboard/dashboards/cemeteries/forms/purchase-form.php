598e129d10ceddba13290a28fb020bca8bc1436d
Malkiel Ben-Ezra <052840@gmail.com>
2025-09-15 13:28:20 +0300

    app18 cemeteries forms v1.21.5

--- dashboard/dashboards/cemeteries/forms/purchase-form.php
+++ dashboard/dashboards/cemeteries/forms/purchase-form.php
@@ -1,5 +1,10 @@
 <?php
 // forms/purchase-form.php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header('Content-Type: text/html; charset=utf-8');
+
 require_once __DIR__ . '/FormBuilder.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/dashboard/dashboards/cemeteries/config.php';
 

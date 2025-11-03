c9b89c1a991ef56937034e1572c199f0be386f16
Malkiel Ben-Ezra <052840@gmail.com>
2025-11-03 19:16:02 +0200

    סיידבר -  v 1.2.55

--- dashboard/dashboards/cemeteries/api/get-config.php
+++ dashboard/dashboards/cemeteries/api/get-config.php
@@ -1,19 +1,19 @@
 <?php
-    // קובץ: api/get-config.php
-    header('Content-Type: application/json; charset=utf-8');
+    // // קובץ: api/get-config.php
+    // header('Content-Type: application/json; charset=utf-8');
 
-    // טען את הקונפיג
-    $config = require_once $_SERVER['DOCUMENT_ROOT'] . '/dashboard/dashboards/cemeteries/config/cemetery-hierarchy-config.php';
+    // // טען את הקונפיג
+    // $config = require_once $_SERVER['DOCUMENT_ROOT'] . '/dashboard/dashboards/cemeteries/config/cemetery-hierarchy-config.php';
 
 
-    $type = $_GET['type'] ?? '';  // cemetery / block / plot...
-    $section = $_GET['section'] ?? '';  // table_columns / form_fields...
+    // $type = $_GET['type'] ?? '';  // cemetery / block / plot...
+    // $section = $_GET['section'] ?? '';  // table_columns / form_fields...
 
-    // החזר את החלק המבוקש
-    echo json_encode([
-        'success' => true,
-        'data' => $config[$type][$section] ?? []
-    ]);
+    // // החזר את החלק המבוקש
+    // echo json_encode([
+    //     'success' => true,
+    //     'data' => $config[$type][$section] ?? []
+    // ]);
 ?>
 
 <?php

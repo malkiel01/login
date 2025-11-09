f0034d805d39f5d6dfe0fcdde2cc087cb67b1c90
Malkiel Ben-Ezra <052840@gmail.com>
2025-11-09 21:56:29 +0200

    סיידבר -  v 1.2.58

--- dashboard/dashboards/cemeteries/forms/form-handler.js
+++ dashboard/dashboards/cemeteries/forms/form-handler.js
@@ -1172,8 +1172,8 @@ const FormHandler = {
                 // אכלס מדינות
                 window.populateCountries();
 
-                // ⭐ הסר ספינר
-                hideSelectSpinner('countryId');
+                // // ⭐ הסר ספינר
+                // hideSelectSpinner('countryId');
                 
                 // הגדר listener לשינוי מדינה
                 countryInput.addEventListener('change', async function() {

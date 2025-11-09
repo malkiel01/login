5017feec9b5b56222138a5f635169610c2658d6e
Malkiel Ben-Ezra <052840@gmail.com>
2025-11-09 11:41:02 +0200

    סיידבר -  v 1.2.56

--- dashboard/dashboards/cemeteries/forms/FormBuilder.php
+++ dashboard/dashboards/cemeteries/forms/FormBuilder.php
@@ -434,7 +434,7 @@
                 }
 
                 /* Loading Spinner */
-                /**
+                
                 @keyframes spin {
                     0% { transform: rotate(0deg); }
                     100% { transform: rotate(360deg); }

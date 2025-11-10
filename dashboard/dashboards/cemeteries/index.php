55a30e6bb3e1d203584b2c3060fa2f651e4649f1
Malkiel Ben-Ezra <052840@gmail.com>
2025-11-10 23:05:43 +0200

    סיידבר -  v 1.2.59

--- dashboard/dashboards/cemeteries/index.php
+++ dashboard/dashboards/cemeteries/index.php
@@ -165,6 +165,7 @@ $paymentTypesConfig = require $_SERVER['DOCUMENT_ROOT'] . '/dashboard/dashboards
     <script src="/dashboard/dashboards/cemeteries/js/responsive.js"></script>
     <script src="/dashboard/dashboards/cemeteries/js/unified-table-renderer.js"></script>
 
+    <script src="/dashboard/dashboards/cemeteries/js/operation-manager.js"></script>
     <script src="/dashboard/dashboards/cemeteries/js/cemeteries-management.js"></script>
     <script src="/dashboard/dashboards/cemeteries/js/blocks-management.js"></script>
     <script src="/dashboard/dashboards/cemeteries/js/plots-management.js"></script>

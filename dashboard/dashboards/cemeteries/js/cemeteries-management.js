502b6c647e1ef6e05f3b8c0748a30a35df87b230
Malkiel Ben-Ezra <052840@gmail.com>
2025-12-10 15:05:18 +0200

    v 2.1.0

--- dashboard/dashboards/cemeteries/js/cemeteries-management.js
+++ dashboard/dashboards/cemeteries/js/cemeteries-management.js
@@ -176,48 +176,48 @@ async function initCemeteriesSearch(signal) {
                 type: 'text',
                 matchType: ['exact', 'fuzzy', 'startsWith']
             },
-            {
-                name: 'cemeteryNameEn',
-                label: 'שם בית עלמין (אנגלית)',
-                table: 'cemeteries',
-                type: 'text',
-                matchType: ['exact', 'fuzzy', 'startsWith']
-            },
-            {
-                name: 'cemeteryCode',
-                label: 'קוד בית עלמין',
-                table: 'cemeteries',
-                type: 'text',
-                matchType: ['exact', 'startsWith']
-            },
-            {
-                name: 'address',
-                label: 'כתובת',
-                table: 'cemeteries',
-                type: 'text',
-                matchType: ['exact', 'fuzzy']
-            },
-            {
-                name: 'contactName',
-                label: 'איש קשר',
-                table: 'cemeteries',
-                type: 'text',
-                matchType: ['exact', 'fuzzy']
-            },
-            {
-                name: 'contactPhoneName',
-                label: 'טלפון',
-                table: 'cemeteries',
-                type: 'text',
-                matchType: ['exact', 'fuzzy']
-            },
-            {
-                name: 'createDate',
-                label: 'תאריך יצירה',
-                table: 'cemeteries',
-                type: 'date',
-                matchType: ['exact', 'before', 'after', 'between', 'today', 'thisWeek', 'thisMonth']
-            }
+            // {
+            //     name: 'cemeteryNameEn',
+            //     label: 'שם בית עלמין (אנגלית)',
+            //     table: 'cemeteries',
+            //     type: 'text',
+            //     matchType: ['exact', 'fuzzy', 'startsWith']
+            // },
+            // {
+            //     name: 'cemeteryCode',
+            //     label: 'קוד בית עלמין',
+            //     table: 'cemeteries',
+            //     type: 'text',
+            //     matchType: ['exact', 'startsWith']
+            // },
+            // {
+            //     name: 'address',
+            //     label: 'כתובת',
+            //     table: 'cemeteries',
+            //     type: 'text',
+            //     matchType: ['exact', 'fuzzy']
+            // },
+            // {
+            //     name: 'contactName',
+            //     label: 'איש קשר',
+            //     table: 'cemeteries',
+            //     type: 'text',
+            //     matchType: ['exact', 'fuzzy']
+            // },
+            // {
+            //     name: 'contactPhoneName',
+            //     label: 'טלפון',
+            //     table: 'cemeteries',
+            //     type: 'text',
+            //     matchType: ['exact', 'fuzzy']
+            // },
+            // {
+            //     name: 'createDate',
+            //     label: 'תאריך יצירה',
+            //     table: 'cemeteries',
+            //     type: 'date',
+            //     matchType: ['exact', 'before', 'after', 'between', 'today', 'thisWeek', 'thisMonth']
+            // }
         ]
     
     try {

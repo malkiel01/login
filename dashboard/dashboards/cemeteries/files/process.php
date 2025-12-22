7f5236da9c10d2bf5f32b89a11ed7cd616eb2fcb
Malkiel Ben-Ezra <052840@gmail.com>
2025-12-22 22:40:01 +0200

    v 2.1.0

--- dashboard/dashboards/cemeteries/files/process.php
+++ dashboard/dashboards/cemeteries/files/process.php
@@ -64,8 +64,19 @@ if (!move_uploaded_file($file['tmp_name'], $input_path)) {
 }
 
 // Call Python script
+// $command = sprintf(
+//     'python3 %s %s %s 2>&1',
+//     escapeshellarg($python_script),
+//     escapeshellarg($input_path),
+//     escapeshellarg($output_path)
+// );
+
+// נתיב ל-Python של venv
+$venv_python = '/home2/mbeplusc/public_html/form/login/venv/bin/python3';
+
 $command = sprintf(
-    'python3 %s %s %s 2>&1',
+    '%s %s %s %s 2>&1',
+    $venv_python,
     escapeshellarg($python_script),
     escapeshellarg($input_path),
     escapeshellarg($output_path)
